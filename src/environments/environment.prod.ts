4d833066db80b44881c81b7f43111c98990d0a8a
igorv-web <igorvilkov404@gmail.com>
2021-01-31 17:55:08 +0200

    Fixed something

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBDFki0yrEPCB4BvBOWWb6_3jAwjsW0l5I",
+    authDomain: "pizza-webdev.firebaseapp.com",
+    projectId: "pizza-webdev",
+    storageBucket: "pizza-webdev.appspot.com",
+    messagingSenderId: "79754090338",
+    appId: "1:79754090338:web:27edf0c8fa51394c24fe3b"
+  },
 };
